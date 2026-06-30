9a7d2865957d8c9b4400e29a37ce813a716d1416
HadiaMasroor-21 <hadiamasroor01@gmail.com>
2026-07-01 01:50:01 +0500

    expense tracker updated

--- script.js
+++ script.js
@@ -10,17 +10,52 @@ const transactionlist = document.getElementById("transaction-list")
 
 const balance = document.getElementById("balance")
 
+let totalIncome = 0;
+let totalExpense = 0;
+
+let transactions = [];
+
+
 form.addEventListener("submit" , function(event){
     event.preventDefault();
     const descriptionvalue = description.value
     const amountvalue = amount.value
     const typevalue = type.value
 
+      if (descriptionvalue === "" || amountvalue === "") {
+        alert("Please fill all fields");
+        return;
+    }
+ transactions.push({
+        description: descriptionvalue,
+        amount: Number(amountvalue),
+        type: typevalue
+    });
+
+    
+
+    if(typevalue == "income"){
+        totalIncome = totalIncome + Number(amountvalue);
+    }
+    else{
+        totalExpense = totalExpense + Number(amountvalue);
+    }
+
+    income.innerText = totalIncome;
+    expense.innerText = totalExpense;
+    balance.innerText = totalIncome - totalExpense;
+
+
+
+
+
+
     console.log(descriptionvalue)
     console.log(amountvalue)
     console.log(typevalue)
 
-    const li = document.createElement("li")
-    li.innerHTML = `${descriptionvalue} <span>Rs. ${amountvalue}</span>`;
+    const li = document.createElement("li")   //for history
+    li.innerHTML = `${descriptionvalue} <span>${typevalue === "income" ? "+" : "-"}Rs. ${amountvalue}</span>`;
+    li.classList.add(typevalue);
     transactionlist.appendChild(li)
 })
